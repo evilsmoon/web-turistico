fb1a8ddc84c9c3b3aad90206121d446396ad8377
danny <dtoctaguano@outlook.com>
2021-05-20 13:15:20 -0500

    revicion

--- src/controllers/authentication.js
+++ src/controllers/authentication.js
@@ -6,12 +6,12 @@ const pool = require('../database');
 const { getToken, getTokenData } = require('../lib/jwt.config');
 const { getTemplate, sendEmail } = require('../lib/mail');
 
-// const cloudinary = require('cloudinary').v2;
-// cloudinary.config({
-//     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-//     api_key: process.env.CLOUDINARY_API_KEY,
-//     api_secret: process.env.CLOUDINARY_API_SECRET
-// });
+const cloudinary = require('cloudinary').v2;
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
 module.exports = {
 
